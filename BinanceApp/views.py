77226d8a468da564a34e5064382ba6d53759dd41
Your Name <development0261@gmail.com>
2021-07-05 14:55:34 +0530

    DMA Logic Change

--- BinanceApp/views.py
+++ BinanceApp/views.py
@@ -3,6 +3,7 @@ from binance import Client
 from django.http import JsonResponse
 from django.shortcuts import render
 import numpy as np
+import pandas
 from multiprocessing.pool import ThreadPool as Pool
 import time
 pool_size = 8
@@ -30,60 +31,44 @@ def worker(item,kinterval):
             klines1 = client.get_historical_klines(item, kinterval, days_25_ago)
             seven_day_avg = 0
             twentyfive_day_avg= 0
-            np_klines = np.array(klines).astype(float)
-            if not np.any(np.isnan(np_klines)):
-                seven_day_avg = np.mean(np_klines, axis=0)
 
+            daily_lines = []
+            for line in klines:
+                daily_lines.append(float(line[4]))
+            df = pandas.DataFrame({"Data": daily_lines})
+            seven_day_avg = df.Data.rolling(window=7).mean().iloc[-1]
 
-            np_klines1 = np.array(klines1).astype(float)
-            if not np.any(np.isnan(np_klines1)):
-                twentyfive_day_avg = np.mean(np_klines1, axis=0)
+            daily_lines1 = []
+            for line in klines1:
+                daily_lines1.append(float(line[4]))
+            df = pandas.DataFrame({"Data": daily_lines1})
+            twentyfive_day_avg = df.Data.rolling(window=25).mean().iloc[-1]
 
 
             global crypto_list
 
 
-            if seven_day_avg[4] > twentyfive_day_avg[4]:
+            if seven_day_avg > twentyfive_day_avg:
 
 
                 crypto_list.append({
                     'Coin':item,
-                    'DMA_7':seven_day_avg[4],
-                    'DMA_25':twentyfive_day_avg[4]
+                    'DMA_7':seven_day_avg,
+                    'DMA_25':twentyfive_day_avg
                 })
 
     except Exception as e:
         pass
 
 def index(request):
-
     return render(request,"index.html")
 
-# def get_coins(request):
-#     exchange_info = client.get_exchange_info()
-#
-#     pool = Pool(pool_size)
-#     print(exchange_info['symbols'][0])
-#     symbols = []
-#     for item in exchange_info['symbols']:
-#         if str(item['quoteAsset']) == "USDT" or str(item['baseAsset']) == "USDT":
-#             symbols.append(item['symbol'])
-#     for item in symbols[:800]:
-#         pool.apply_async(worker, (item,))
-#
-#
-#     pool.close()
-#     pool.join()
-#
-#     return JsonResponse({'msg':"Success","coins":crypto_list})
+
 
 def get_coin_price(request):
-
     global crypto_list
-
     crypto_list = []
     interval = request.POST['interval']
-
     kinterval = ''
     if interval == "1Hour":
         kinterval = Client.KLINE_INTERVAL_1HOUR
@@ -92,26 +77,21 @@ def get_coin_price(request):
     elif interval == "1Day":
         kinterval = Client.KLINE_INTERVAL_1DAY
     elif interval == "1Week":
-
         kinterval = Client.KLINE_INTERVAL_1WEEK
-
-    exchange_info = client.get_exchange_info()
-
+    symbols_fetched = client.get_all_tickers()
+    print(len(symbols_fetched))
     pool = Pool(pool_size)
-
     symbols = []
-    for item in exchange_info['symbols'][:900]:
-        if str(item['quoteAsset']) == "USDT" or str(item['baseAsset']) == "USDT":
+    for item in symbols_fetched:
+        if str(item['symbol'])[-4:] == "USDT":
+            if(item['symbol'] == 'DOGEUSDT'):
+                print("DOGE")
             symbols.append(item['symbol'])
     for item in symbols:
         pool.apply_async(worker, (item,kinterval))
-
     pool.close()
     pool.join()
-
     coin_price = []
-
-
     try:
 
         for i in range(0, len(crypto_list)):
@@ -120,13 +100,9 @@ def get_coin_price(request):
             coin_data['DMA_7'] = crypto_list[i]['DMA_7']
             coin_data['DMA_25'] = crypto_list[i]['DMA_25']
             coin_price.append(coin_data)
-
-
-
-
     except:
         pass
-    print(coin_price)
+
     return JsonResponse({'msg':'Success','coin_prices':coin_price})
 
 
--- BinanceTest.py
+++ BinanceTest.py
@@ -1,22 +1,28 @@
-import asyncio
-from binance import AsyncClient, BinanceSocketManager
-from binance.enums import KLINE_INTERVAL_30MINUTE
+import datetime
+
+import numpy as np
+from binance import Client
+import pandas
+
+BINANCE_API_KEY = "Bs0hCk8zsE6IteGIyXLPVBGEnYGhcBTjjWJfVMKSZFU5YSwiAMhx2rc1ICRcWkOa"
+BINANCE_SECRET_KEY = "D8UdEhDFB61BSH09Kuqlrt2IAjGKPJ0I2Ok2JGzwenUCOHskQmRSqMdh3WWtaTvJ"
 
 
-async def main():
-    client = await AsyncClient.create()
-    bm = BinanceSocketManager(client)
-    # start any sockets here, i.e a trade socket
-    ts = bm.kline_socket('BNBBTC',interval=KLINE_INTERVAL_30MINUTE)
-    # then start receiving messages
-    async with ts as tscm:
-        while True:
-            res = await tscm.recv()
-            print(res)
+client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
+today_date = datetime.datetime.today().date()
+today_date = today_date.strftime("%d %b, %Y")
+one_week_ago = datetime.datetime.today().date() - datetime.timedelta(days=7)
+one_week_ago = one_week_ago.strftime("%d %b, %Y")
+days_25_ago = datetime.datetime.today().date() - datetime.timedelta(days=25)
+days_25_ago = days_25_ago.strftime("%d %b, %Y")
+klines = client.get_historical_klines("DOGEUSDT", Client.KLINE_INTERVAL_4HOUR, one_week_ago,today_date)
+klines1 = client.get_historical_klines("DOGEUSDT", Client.KLINE_INTERVAL_4HOUR, days_25_ago)
+seven_day_avg = 0
+twentyfive_day_avg = 0
 
-    await client.close_connection()
+daily_lines = []
+for line in klines:
+    daily_lines.append(float(line[4]))
+df = pandas.DataFrame({"Data":daily_lines})
+print(df.Data.rolling(window=25).mean().iloc[-1])
 
-if __name__ == "__main__":
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
